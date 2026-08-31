3231d5ad9133279c5b9530861b283c33bfda2845
Devpaul-01 <oluwaseyiogunsola90@gmail.com>
2026-08-31 05:20:33 +0100

    feat: add requested_at timestamp to ThreadJoinRequest model
    - Add requested_at DateTime column to track when join request was made
    - Enables proper sorting and auditing of thread join requests

--- models.py
+++ models.py
@@ -1037,6 +1037,7 @@ class ThreadJoinRequest(db.Model):
     message     = db.Column(db.Text)
     status      = db.Column(db.Enum(ThreadJoinRequestStatus, native_enum=False, values_callable=lambda e: [m.value for m in e]), default=ThreadJoinRequestStatus.PENDING.value, index=True)
     reviewed_at = db.Column(db.DateTime)
+    requested_at = db.Column(db.DateTime)
     reviewed_by = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     __table_args__ = (
